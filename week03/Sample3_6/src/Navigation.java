1b9412bfd36384d67a212da109140fa5a1368f35
dithmaa <namutobaozi@gmail.com>
2026-03-20 11:05:45 +0900

    4주차

---
+++ week03/Sample3_6/src/Navigation.java
@@ -0,0 +1,11 @@
+public class Navigation {
+    protected String model;
+
+    public Navigation(String model) {
+        this.model = model;
+    }
+
+    public void findRoute(String start, String destination) {
+        System.out.println();
+    }
+}
